2f0132d9a277b7baafd93e346e4f56c5809a06b9
Stefan Siegl <stesie@brokenpipe.de>
2016-12-27 11:44:57 +0100

    re-design event editor; allow to choose event source

--- app/src/main/java/de/brokenpipe/taskermqtt/BundleExtraKeys.java
+++ app/src/main/java/de/brokenpipe/taskermqtt/BundleExtraKeys.java
@@ -3,4 +3,9 @@ package de.brokenpipe.taskermqtt;
 class BundleExtraKeys {
     static final String FILTER_TOPIC = "Topic";
     static final String FILTER_PAYLOAD = "Payload";
+    static final String EVENT_SOURCE = "EventSource";
+
+    static final String EVENT_SOURCE_MESSAGE_RECEIVED = "MessageReceived";
+    static final String EVENT_SOURCE_CONNECTED = "Connected";
+    static final String EVENT_SOURCE_DISCONNECTED = "Disconnected";
 }
--- app/src/main/java/de/brokenpipe/taskermqtt/EditEventActivity.java
+++ app/src/main/java/de/brokenpipe/taskermqtt/EditEventActivity.java
@@ -4,9 +4,12 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.EditText;
+import android.widget.Spinner;
 
 public class EditEventActivity extends Activity {
+    private Spinner eventSourceSpinner;
     private EditText filterTopicText, filterPayloadText;
 
     @Override
@@ -16,44 +19,95 @@ public class EditEventActivity extends Activity {
         final Bundle localeBundle = getIntent().getBundleExtra(com.twofortyfouram.locale.Intent.EXTRA_BUNDLE);
 
         setContentView(R.layout.edit_event);
+        eventSourceSpinner = (Spinner) findViewById(R.id.eventSource);
         filterTopicText = (EditText) findViewById(R.id.filter_topic);
         filterPayloadText = (EditText) findViewById(R.id.filter_payload);
 
-        if (savedInstanceState == null && localeBundle != null) {
-            filterTopicText.setText(localeBundle.getString(BundleExtraKeys.FILTER_TOPIC));
-            filterPayloadText.setText(localeBundle.getString(BundleExtraKeys.FILTER_PAYLOAD));
+        eventSourceSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                boolean enableFilters = position == 0;
+                filterTopicText.setEnabled(enableFilters);
+                filterPayloadText.setEnabled(enableFilters);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+        if (savedInstanceState != null || localeBundle == null)
+            return;
+
+        final String eventSource = localeBundle.getString(BundleExtraKeys.EVENT_SOURCE);
+
+        if (eventSource == null)
+            return;
+
+        switch (eventSource) {
+            case BundleExtraKeys.EVENT_SOURCE_MESSAGE_RECEIVED:
+                this.eventSourceSpinner.setSelection(0);
+                break;
+
+            case BundleExtraKeys.EVENT_SOURCE_CONNECTED:
+                this.eventSourceSpinner.setSelection(1);
+                break;
+
+            case BundleExtraKeys.EVENT_SOURCE_DISCONNECTED:
+                this.eventSourceSpinner.setSelection(2);
+                break;
         }
+
+        filterTopicText.setText(localeBundle.getString(BundleExtraKeys.FILTER_TOPIC, ""));
+        filterPayloadText.setText(localeBundle.getString(BundleExtraKeys.FILTER_PAYLOAD, ""));
     }
 
     public void finishActivity(View view) {
         final String topicText = filterTopicText.getText().toString().trim();
         final String payloadText = filterPayloadText.getText().toString().trim();
 
-        Bundle bundle = new Bundle();
-        bundle.putString(BundleExtraKeys.FILTER_TOPIC, topicText);
-        bundle.putString(BundleExtraKeys.FILTER_PAYLOAD, payloadText);
-
         Intent resultIntent = new Intent();
-        resultIntent.putExtra(com.twofortyfouram.locale.Intent.EXTRA_BUNDLE, bundle);
-
-        // We define the blurb that will appear in the configuration
+        Bundle bundle = new Bundle();
         StringBuilder blurb = new StringBuilder();
 
-        blurb.append("Topic: ");
-        blurb.append(topicText.equals("") ? "*" : topicText);
+        blurb.append("Event Source: ");
+        blurb.append(eventSourceSpinner.getSelectedItem().toString());
         blurb.append("\n");
 
-        blurb.append("Payload: ");
-        blurb.append(payloadText.equals("") ? "*" : payloadText);
 
-        resultIntent.putExtra(com.twofortyfouram.locale.Intent.EXTRA_STRING_BLURB, blurb.toString());
+        switch (eventSourceSpinner.getSelectedItemPosition()) {
+            case 0:
+                bundle.putString(BundleExtraKeys.EVENT_SOURCE, BundleExtraKeys.EVENT_SOURCE_MESSAGE_RECEIVED);
+                bundle.putString(BundleExtraKeys.FILTER_TOPIC, topicText);
+                bundle.putString(BundleExtraKeys.FILTER_PAYLOAD, payloadText);
 
-        if (TaskerPlugin.hostSupportsRelevantVariables(getIntent().getExtras())) {
-            TaskerPlugin.addRelevantVariableList(resultIntent, new String [] {
-                    "%mqtopic\nMQTT Topic\nThe topic the message was received on",
-                    "%mqpayload\nMQTT Payload\nThe payload of the message that was received"
-            });
+                blurb.append("Topic: ");
+                blurb.append(topicText.equals("") ? "*" : topicText);
+                blurb.append("\n");
+
+                blurb.append("Payload: ");
+                blurb.append(payloadText.equals("") ? "*" : payloadText);
+
+                if (TaskerPlugin.hostSupportsRelevantVariables(getIntent().getExtras())) {
+                    TaskerPlugin.addRelevantVariableList(resultIntent, new String [] {
+                            "%mqtopic\nMQTT Topic\nThe topic the message was received on",
+                            "%mqpayload\nMQTT Payload\nThe payload of the message that was received"
+                    });
+                }
+                break;
+
+            case 1:
+                bundle.putString(BundleExtraKeys.EVENT_SOURCE, BundleExtraKeys.EVENT_SOURCE_CONNECTED);
+                break;
+
+            case 2:
+                bundle.putString(BundleExtraKeys.EVENT_SOURCE, BundleExtraKeys.EVENT_SOURCE_DISCONNECTED);
+                break;
         }
+
+        resultIntent.putExtra(com.twofortyfouram.locale.Intent.EXTRA_BUNDLE, bundle);
+        resultIntent.putExtra(com.twofortyfouram.locale.Intent.EXTRA_STRING_BLURB, blurb.toString());
 
         setResult(RESULT_OK, resultIntent);
         super.finish();
